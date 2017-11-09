ffb72af4fb5168376a99b91553b82b743f9e7012
Taranveer Virk <taranveer@virk.cc>
2017-11-09 11:07:36 -0500

    fix failing build

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -3,4 +3,10 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-// export * from './src';
+import {expect} from '@loopback/testlab';
+
+describe('Smoke test to verify project setup - remove me later', () => {
+  it('works', () => {
+    expect(true).to.equal(true);
+  });
+});
