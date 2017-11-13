145b30fad6a2b0a66fbfe4e0a9157deb79370061
Taranveer Virk <taranveer@virk.cc>
2017-11-13 16:30:10 -0500

    refactor to be more debuggable

--- src/providers/log-action.provider.ts
+++ src/providers/log-action.provider.ts
@@ -21,51 +21,15 @@ export class LogActionProvider implements Provider<LogFn> {
     @inject(EXAMPLE_LOG_BINDINGS.TIMER) public timer: TimerFn,
   ) {}
 
-  value() {
-    const fn = <LogFn>(async (
+  value(): LogFn {
+    const fn = <LogFn>((
       req: ParsedRequest,
       args: OperationArgs,
       // tslint:disable-next-line:no-any
       result: any,
       start?: [number, number],
     ) => {
-      const controllerClass = await this.getController();
-      const methodName: string = await this.getMethod();
-
-      const level: number = getLogMetadata(controllerClass, methodName);
-
-      if (
-        this.logLevel !== LOG_LEVEL.OFF &&
-        level >= this.logLevel &&
-        level !== LOG_LEVEL.OFF
-      ) {
-        if (!args) args = [];
-        let log = `${req.url} :: ${controllerClass.name}.`;
-        log += `${methodName}(${args.join(', ')}) => `;
-
-        if (typeof result === 'object') log += JSON.stringify(result);
-        else log += result;
-
-        if (start) {
-          const time = this.timer(start);
-          log = `${time}ms: ${log}`;
-        }
-
-        switch (level) {
-          case LOG_LEVEL.DEBUG:
-            console.log(`\x1b[37m DEBUG: ${log} \x1b[0m`);
-            break;
-          case LOG_LEVEL.INFO:
-            console.log(`\x1b[32m INFO: ${log} \x1b[0m`);
-            break;
-          case LOG_LEVEL.WARN:
-            console.log(`\x1b[33m WARN: ${log} \x1b[0m`);
-            break;
-          case LOG_LEVEL.ERROR:
-            console.log(`\x1b[31m ERROR: ${log} \x1b[0m`);
-            break;
-        }
-      }
+      return this.action(req, args, result, start);
     });
 
     fn.startTimer = () => {
@@ -73,5 +37,51 @@ export class LogActionProvider implements Provider<LogFn> {
     };
 
     return fn;
+  }
+
+  private async action(
+    req: ParsedRequest,
+    args: OperationArgs,
+    // tslint:disable-next-line:no-any
+    result: any,
+    start?: [number, number],
+  ) {
+    const controllerClass = await this.getController();
+    const methodName: string = await this.getMethod();
+
+    const level: number = getLogMetadata(controllerClass, methodName);
+
+    if (
+      this.logLevel !== LOG_LEVEL.OFF &&
+      level >= this.logLevel &&
+      level !== LOG_LEVEL.OFF
+    ) {
+      if (!args) args = [];
+      let log = `${req.url} :: ${controllerClass.name}.`;
+      log += `${methodName}(${args.join(', ')}) => `;
+
+      if (typeof result === 'object') log += JSON.stringify(result);
+      else log += result;
+
+      if (start) {
+        const time = this.timer(start);
+        log = `${time}ms: ${log}`;
+      }
+
+      switch (level) {
+        case LOG_LEVEL.DEBUG:
+          console.log(`\x1b[37m DEBUG: ${log} \x1b[0m`);
+          break;
+        case LOG_LEVEL.INFO:
+          console.log(`\x1b[32m INFO: ${log} \x1b[0m`);
+          break;
+        case LOG_LEVEL.WARN:
+          console.log(`\x1b[33m WARN: ${log} \x1b[0m`);
+          break;
+        case LOG_LEVEL.ERROR:
+          console.log(`\x1b[31m ERROR: ${log} \x1b[0m`);
+          break;
+      }
+    }
   }
 }
