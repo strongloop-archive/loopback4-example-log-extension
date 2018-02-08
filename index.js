f8418f440aa2a769c1b52019db132a10d905e1dd
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-02-08 15:51:26 +0100

    Add a deprecation message pointing to our monorepo

--- index.js
+++
@@ -1,8 +0,0 @@
-// Copyright IBM Corp. 2017. All Rights Reserved.
-// Node module: loopback4-example-log-extension
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-const dist = nodeMajorVersion >= 7 ? './dist' : './dist6';
-module.exports = require(dist);
